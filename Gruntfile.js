19db09cbb52a73cd23f8a701c86235a721fc86d7
zhangchen <zhangchen2397@126.com>
2014-08-15 00:41:04 +0800

    add grunt

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/* <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %>\n' + 
+                  ' * version: <%= pkg.version %>\n' +
+                  ' * @author: <%= pkg.email %>\n' +
+                  ' */\n'
+
+      },
+      build: {
+        src: '<%= pkg.name %>.js',
+        dest: '<%= pkg.name %>.min.js'
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+};
